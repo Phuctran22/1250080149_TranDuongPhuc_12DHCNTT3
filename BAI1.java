58571dbaab1e64ccea53fa759dae54e4012beedd
Phuctran22 <phuctran212005@gmail.com>
2025-02-24 15:16:00 +0700

    Add files via upload

---
+++ BAI1.java
@@ -0,0 +1,18 @@
+package LAP1;
+
+import java.util.Scanner;
+
+public class BAI1 {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        
+        String name = "Tran Duong Phuc";
+        String ngsinh = "21/02/2005";
+        String mssv = "1250080149";
+
+        System.out.println("Ten: " + name);
+        System.out.println("Ngày Sinh: " + ngsinh);
+        System.out.println("Ma so sinh vien: " + mssv);
+    }
+    }
+
